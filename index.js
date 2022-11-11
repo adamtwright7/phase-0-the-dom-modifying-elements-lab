6bfe5ebc072cbf1904bc0ddb751e02940233e99f
Adam Wright <adamtwright7@gmail.com>
2022-11-10 22:19:20 -0500

    commit tree 89e4479f9e861422132d987487e751dccea80590

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+let mainElement = document.querySelector('main');
+mainElement.remove();
+
+let newHeader = document.createElement('h1');
+newHeader.setAttribute('id','victory');
+newHeader.textContent = 'Adam is the champion';
